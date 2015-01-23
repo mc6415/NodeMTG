c28a8b993662308cdf2e698b36d90cda3335e126
mc6415 <mc6415@gmail.com>
2015-01-23 01:46:32 +0000

    Added cards

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,8 @@ function getSets(req,res,callback){
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.clearCookie('set');
+
   getSets(req,res,function(sets){
     res.render('index', {title:'Portal', sets:sets})
   })
@@ -20,11 +22,35 @@ router.get('/', function(req, res) {
 
 router.post('/cards', function(req,res){
     var cardName = req.body.cardName;
+    var rarity = req.param('rarity');
+    if(rarity===undefined){
+      rarity = 'all'
+    }
+    console.log('Rarity: '+rarity);
     var card = tutor.set(cardName, function(err,cards){
       console.log(cards[0]);
-      res.render('cards', {title:cardName, cards:cards})
+      res.render('cards', {title:cardName, cards:cards, setName: cardName, rarity:rarity})
     })
 
+})
+
+router.post('/getcard', function(req,res){
+  var cardName = req.body.card;
+  res.redirect('/card?name='+cardName)
+})
+
+router.get('/card', function(req,res){
+  var cardName = req.param('name');
+  tutor.card(cardName, function(err,card){
+    tutor.formats(function(err, formatNames){
+      console.log(card);
+      res.render('card', {
+        title: card.name,
+        card: card,
+        formats: formatNames
+      })
+    })
+  })
 })
 
 module.exports = router;
